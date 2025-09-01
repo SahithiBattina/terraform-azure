0ace0677778bc712fec14b7bb2763a1029becf9b
Sahithi <sahithibattina28@gmail.com>
2025-09-01 17:26:49 -0500

    Add 10 Azure resources for demo

--- main.tf
+++ main.tf
@@ -24,3 +24,31 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "myVM"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  tags = {
+    environment = "Testing"
+  }
+}
+
